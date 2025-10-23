33388f574102fef91970a3d1d6b4029b05c2aa08
eviled7 <erikdamonpv@gmail.com>
2025-10-23 07:59:48 -0700

    my branch

---
+++ Assets/tst/Erik/Tests/PlayMode/SpawnTesting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+
+public class SpawnTesting
+{
+    private ObjectSpawner spawner;
+    private LevelManager manager;
+
+    [UnitySetup]
+    public IEnumerator LoadScene()
+    {
+        SceneManager.LoadScene("LevelLoadTest");
+        yield return null;
+    }
+
+    [SetUp]
+    public void Setup() {
+        // spawner = ScriptableObject.CreateInstance<ObjectSpawner>();
+        // prefab = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+    }
+
+    [UnityTest]
+
+    public IEnumerator StressSpawn_Prefabs_Continuous() {
+        while(true) {
+            
+        }
+    }
+}
