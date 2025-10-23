20385ad1f960ccdad390476fe647f444eba29366
ichigojin <ky246.code@gmail.com>
2025-10-22 23:53:29 -0700

    Add tests for enemies (Proper way)

---
+++ Assets/tst/Kynan/Playmode/CanReadTerrainTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class CanReadTerrainTest {
+  private EnemyHub enemyHubScript;
+
+  [UnitySetUp]
+  public IEnumerator UnitySetup(){
+    yield return new WaitForFixedUpdate();
+    SceneManager.LoadSceneAsync("SampleScene");
+    yield return new WaitForSeconds(1f);
+
+    GameObject enemyHub = GameObject.Find("Enemy Hub");
+    enemyHubScript = enemyHub.GetComponent<EnemyHub>();
+
+    yield return null;
+  }
+
+  [UnityTest]
+  public IEnumerator SpawnTestWithEnumeratorPasses(){
+    if(enemyHubScript.runTests("CHECK_TERRAIN_EXISTS") > 0){
+      Assert.Fail("Failed to read the terrain. The terrain may not exist, may not be in the terrain layer, or the function is broken.");
+    }
+    yield return null;
+  }
+}
